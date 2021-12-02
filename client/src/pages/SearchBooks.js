e924d14efa4c7f8c58c19afce5008f7c10b1b6ca
Hungry Man <katsu.matsushita@gmail.com>
2021-12-01 23:06:46 -0500

    SearchBooks refactored SearchBooks.js has been refactored to use mutations instead of API

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+// import mutation hook per instructions
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
@@ -13,6 +16,9 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  // set up the mutation hook
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,7 +71,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      // per the instructions, replace the API saveBook with the mutation saveBook
+      const response = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
